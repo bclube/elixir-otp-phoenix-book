2bb13dc1d20dfe43301988904202ac16248439ed
Brian Lubeski <bclube@gmail.com>
2017-12-04 12:33:38 -0500

    Modify IslandsEngine.Guesses.add syntax to match book

--- islands_engine/lib/islands_engine/guesses.ex
+++ islands_engine/lib/islands_engine/guesses.ex
@@ -2,8 +2,7 @@ defmodule IslandsEngine.Guesses do
   @moduledoc """
   Data structure for keeping track of guesses made so far in the game.
   """
-  alias __MODULE__
-  alias IslandsEngine.Coordinate
+  alias IslandsEngine.{Coordinate, Guesses}
 
   @enforce_keys [:hits, :misses]
   defstruct [
@@ -11,18 +10,21 @@ defmodule IslandsEngine.Guesses do
     :misses
   ]
 
+  @doc """
+  Creates a new Guesses data structure ready to start tracking guesses.
+  """
   def new, do:
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
 
   @doc """
   Adds coordinate as a hit in `guesses`.
   """
-  def add_hit(%__MODULE__{}=guesses, %Coordinate{}=coord), do:
+  def add(%Guesses{} = guesses, :hit, %Coordinate{} = coord), do:
     update_in(guesses.hits, &MapSet.put(&1, coord))
 
   @doc """
   Adds coordinate as a miss in `guesses`.
   """
-  def add_miss(%__MODULE__{}=guesses, %Coordinate{}=coord), do:
+  def add(%Guesses{} = guesses, :miss, %Coordinate{} = coord), do:
     update_in(guesses.misses, &MapSet.put(&1, coord))
 end
